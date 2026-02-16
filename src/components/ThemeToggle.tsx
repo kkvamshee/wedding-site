b5aa5cbaa6b36702a5432478e6217017adb546b0
Kabeer Vamshee Krishna <kkvamshee@gmail.com>
2026-02-16 08:31:05 +0800

    handling state of color-picker

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -31,8 +31,7 @@ const ThemeToggle = () => {
       <input 
         type="color" 
         className="p-1 h-10 w-14 block bg-layer border border-layer-line cursor-pointer rounded-lg disabled:opacity-50 disabled:pointer-events-none" 
-        id="hs-color-input" 
-        value="hsl(240, 5, 10)" 
+        id="hs-color-input"  
         title="Choose your color"
         onChange={e => onColorChange(e)}
       >
